fe7777c69bf963c24aad30862c4718328a5beb91
malik <zeryab.malik64@myhunter.cuny.edu>
2019-05-21 10:20:30 -0400

    final upload

---
+++ temp/scripts/app.js
@@ -0,0 +1,48 @@
+/*jslint browser, es6 */
+
+/*global window */
+window.onload = function () {
+  const CSSPreProcessor = `Sass`;
+  const JSVersion = `ES6`;
+  let body = document.querySelector(`body`);
+  let paragraph = document.createElement(`p`);
+  paragraph.appendChild(text);
+  body.appendChild(paragraph);
+};
+
+var slideIndex = 1;
+showSlides(slideIndex); // Next/previous controls
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+} // Thumbnail image controls
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
